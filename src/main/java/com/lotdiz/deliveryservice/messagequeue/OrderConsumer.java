a907e1cfd3e8e0c94abca06be017246ca75ab4cb
CokeLee777 <akak640@kyonggi.ac.kr>
2023-10-06 11:42:24 +0900

    remove: remove unused annotation

--- src/main/java/com/lotdiz/deliveryservice/messagequeue/OrderConsumer.java
+++ src/main/java/com/lotdiz/deliveryservice/messagequeue/OrderConsumer.java
@@ -5,12 +5,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lotdiz.deliveryservice.dto.request.CreateDeliveryRequestDto;
 import com.lotdiz.deliveryservice.service.DeliveryService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Service;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class OrderConsumer {
